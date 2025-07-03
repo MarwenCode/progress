a02e4fa1e39e52fe8eda457d1b5656973f148bd3
MarwenCode <marwen_jdidi@yahoo.com>
2025-07-03 22:20:39 +0200

    edit the google config

--- api/config/google.config.js
+++ api/config/google.config.js
@@ -17,6 +17,6 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 export const googleConfig = {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/api/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     scope: ['profile', 'email']
 }; 
